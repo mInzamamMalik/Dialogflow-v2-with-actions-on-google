d5c8830d075de31b18d8860794010b347f7dbcd5
Mohammad Inzamam Malik <malikasinger1@gmail.com>
2018-09-04 17:56:38 +0500

    soe code commented for less error

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as functions from 'firebase-functions';
 import { dialogflow, SimpleResponse, Suggestions, DialogflowConversation, DialogflowApp } from 'actions-on-google'
 import { raw } from './core'
+import { userEntity } from "./helperfunctions"
 
 const app = dialogflow({ debug: false })
 
@@ -25,16 +26,26 @@ app.intent('Default Welcome Intent', (conv) => {
 });
 
 
-app.intent('Book Hotel', (conv, params) => {
+app.intent('Book Hotel', (conv, params:any) => {
 
-    console.log("params.numberOfPeople: ", params.numberOfPeople)
-    console.log("params.geoCity: ", params.geoCity)
+    console.log("params.name: ", params.name)
+    console.log("params.recipientsname: ", params.recipientsname)
+    console.log("params.characteristics: ", params.characteristics)
+
     console.log("params: ", params)
 
-    if (!params.geoCity) {
-        conv.ask("in which city?")
-    } else if (!params.numberOfPeople) {
-        conv.ask("how many people are you?")
+    if (!params.name) {
+        conv.ask("what is your name?")
+    } else if (!params.recipientsname) {
+        conv.ask("what is your partner name?")
+    } else if (params.characteristics.length == 0) {
+
+        userEntity.makeUserEntityWithArray(raw.request.body.session, "characteristics", ["bold", "clever"]).then(() => {
+            conv.ask("what is your partner characteristics?")
+        }).catch((e) => {
+            console.log("error making user entity")
+        })
+
     } else {
         conv.close(`your hotel is booked for ${params.numberOfPeople} person in ${params.geoCity} city`)
     }
@@ -60,4 +71,37 @@ export const webhook = functions.https.onRequest((request, response) => {
     app(request, response) // handing over request to dialogflow app
 });
 
-// 
+
+
+// var a = {
+//     responseId: 'bea5b994-4361-4a27-84a7-3b1f13cb4e96',
+//     queryResult: {
+//         queryText: 'GOOGLE_ASSISTANT_WELCOME',
+//         action: 'input.welcome',
+//         parameters: {},
+//         allRequiredParamsPresent: true,
+//         fulfillmentMessages: [[Object], [Object]],
+//         outputContexts: [[Object], [Object], [Object], [Object], [Object], [Object]],
+//         intent:
+//         {
+//             name: 'projects/upworkbot-65288/agent/intents/1267b3da-1e10-41b0-9ba5-4ca46698efbf',
+//             displayName: 'Default Welcome Intent'
+//         },
+//         intentDetectionConfidence: 1,
+//         languageCode: 'en-us'
+//     },
+//     originalDetectIntentRequest: {
+//         source: 'google',
+//         version: '2',
+//         payload:
+//         {
+//             isInSandbox: true,
+//             surface: [Object],
+//             requestType: 'SIMULATOR',
+//             inputs: [Object],
+//             user: [Object],
+//             conversation: [Object]
+//         }
+//     },
+//     session: 'projects/upworkbot-65288/agent/sessions/ABwppHHk6DQXdjNVdCmsxxdHkp3WaslqbaYmu2zAVOdS-Xr_C12fWWZubVOnJGbT5wMYcoThPSXA7mVjWwCk'
+// }
