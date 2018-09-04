5adc43a7103dd7102e42171b53fe1c97b0855a4a
Mohammad Inzamam Malik <malikasinger1@gmail.com>
2018-09-04 20:41:37 +0500

    es6

--- functions/src/rough.js
+++ functions/src/rough.js
@@ -2,23 +2,15 @@
 const { google } = require('googleapis');
 
 
-const serviceAccountAuth = new google.auth.JWT({
-    email: "dialogflow-opimvo@upworkbot-65288.iam.gserviceaccount.com",
-    keyFile: "./../service-account.json",
-    scopes: ['https://www.googleapis.com/auth/calendar','https://www.googleapis.com/auth/cloud-platform']
-})
-// .then(data => {
+let getToken = async () => {
 
-//     console.log("data: ", data)
-// })
-
-console.log("serviceAccountAuth: ", serviceAccountAuth)
-
-serviceAccountAuth.authorize().then((data) => {
+    const serviceAccountAuth = new google.auth.JWT({
+        email: "dialogflow-opimvo@upworkbot-65288.iam.gserviceaccount.com",
+        keyFile: "./../service-account.json",
+        scopes: ['https://www.googleapis.com/auth/calendar', 'https://www.googleapis.com/auth/cloud-platform']
+    })
+    return await serviceAccountAuth.authorize()
+}
+getToken().then(data => {
     console.log("data: ", data)
-
-}).catch(e => {
-
-    console.log("e: ", e)
 })
-
